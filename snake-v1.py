d94528272e82cdf48320bdcf047a982075ae87ae
nynaphe-ux <nynaphe@gmail.com>
2026-05-16 19:46:10 +0200

    Add files via upload

--- snake-v1.py
+++ snake-v1.py
@@ -13,11 +13,19 @@ tk = Tk()
 # Les trois autres paramètres permettent de spécifier la taille et la couleur
 # de fond du canevas
 can = Canvas(tk, width=500, height=500, bg='black')
-
+def computeNextFrame(numframe,coordonnee):
+    print(numframe)
+    numframe= numframe + 1
+    can.delete('all')
+    if coordonnee <= 0:
+        coordonnee = 0
+    else:
+        coordonnee += -20
+    can.create_rectangle(coordonnee,200, coordonnee +20 , 220, outline='yellow',fill='red')
+    tk.after(100, lambda:computeNextFrame(numframe,coordonnee))
 # On affiche le canevas
 can.pack()
-can.create_rectangle(10, 30, 10, 60, outline='yellow', fill='green')
-can.create_oval(100, 200, 120, 120, outline= 'red', fill='blue')
+computeNextFrame(0, 500)
 # lancement de la boucle principale qui écoute les évènements (claviers...)
 tk.mainloop() # Cet appel doit être la derniere instruction du programme
 
